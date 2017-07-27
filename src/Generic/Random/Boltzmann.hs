4b88e681922db25b782bb5d8f2fc2780cff6d832
lyxia <lysxia@gmail.com>
2017-07-27 23:51:08 +0200

    Remove Boltzmann samplers
    Moved to separate package boltzmann-samplers

--- src/Generic/Random/Boltzmann.hs
+++
@@ -1,7 +0,0 @@
-
-module Generic.Random.Boltzmann
-  {-# DEPRECATED "Directly use \"Boltzmann.Species\" from @boltzmann-samplers@ instead." #-}
-  ( module Boltzmann.Species
-  ) where
-
-import Boltzmann.Species
--- src/Generic/Random/Data.hs
+++
@@ -1,6 +0,0 @@
-module Generic.Random.Data
-  {-# DEPRECATED "Directly use \"Boltzmann.Data\" from @boltzmann-samplers@ instead." #-}
-  ( module Boltzmann.Data
-  ) where
-
-import Boltzmann.Data
