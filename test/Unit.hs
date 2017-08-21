24665d783702e41137cdc7a0c5d854eadfb41670
lyxia <lysxia@gmail.com>
2017-08-21 19:48:17 +0200

    Add test for new base case search

---
+++ test/Unit.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+import GHC.Generics
+import Test.QuickCheck
+
+import Generic.Random.Generic
+
+newtype T a = W a deriving (Generic, Show)
+
+instance (Arbitrary a, BaseCase (T a)) => Arbitrary (T a) where
+  arbitrary = genericArbitrary' uniform
+
+type instance Found (T a) z = GFound' (T a) z
+instance GBaseCaseSearch (T a) z e => BaseCaseSearch (T a) z e
+
+f :: Gen (T (T Int))
+f = arbitrary
+
+main :: IO ()
+main = sample' f >>= force
+  where
+    force [] = return ()
+    force (x : xs) = x `seq` force xs
