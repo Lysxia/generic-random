73448c23a2fb8344d2f44f9db58e91a26cbbd5bf
Lysxia <lysxia@gmail.com>
2018-05-29 19:13:57 -0400

    Add termination test for genericArbitraryU' with single-field constructor

--- test/Unit.hs
+++ test/Unit.hs
@@ -1,29 +1,46 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
-#if __GLASGOW_HASKELL__ < 710
-{-# LANGUAGE OverlappingInstances #-}
-#endif
 
-import GHC.Generics
+import Control.Monad (replicateM)
+import Control.DeepSeq (NFData, force)
+import GHC.Generics (Generic)
+import System.Timeout (timeout)
+
 import Test.QuickCheck
 
-import Generic.Random.Generic
+import Generic.Random
 
 newtype T a = W a deriving (Generic, Show)
 
 instance (Arbitrary a, BaseCase (T a)) => Arbitrary (T a) where
   arbitrary = genericArbitrary' uniform
 
+instance NFData a => NFData (T a)
+
 f :: Gen (T (T Int))
 f = arbitrary
 
+
+data NTree = Leaf | Node [NTree] deriving (Generic, Show)
+
+instance Arbitrary NTree where
+  arbitrary = genericArbitraryU'
+
+instance NFData NTree
+
+eval :: NFData a => String -> Gen a -> IO ()
+eval name g = do
+  x <- timeout (10 ^ 6) $ do
+    xs <- replicateM 100 (generate g)
+    return $! force xs
+  case x of
+    Just _ -> return ()
+    Nothing -> fail $ name ++ ": did not finish on time"
+
 main :: IO ()
-main = sample' f >>= force
-  where
-    force [] = return ()
-    force (x : xs) = x `seq` force xs
+main = do
+  eval "T" (arbitrary :: Gen (T (T Int)))
+  eval "NTree" (resize 13 arbitrary :: Gen NTree)
