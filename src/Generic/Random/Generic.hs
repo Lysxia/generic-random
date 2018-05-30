2a175e956182038cd0c455dcca9b1f5196eb6041
Lysxia <lysxia@gmail.com>
2018-05-30 14:27:21 -0400

    Remove Generic.Random.Generic

--- src/Generic/Random/Generic.hs
+++
@@ -1,7 +0,0 @@
--- | Reexport of "Generic.Random", for backwards-compatibility.
-
-module Generic.Random.Generic
-       {-# DEPRECATED "Use Generic.Random instead" #-}
-       ( module Generic.Random ) where
-
-import Generic.Random
